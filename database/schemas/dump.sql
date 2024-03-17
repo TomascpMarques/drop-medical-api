83b68dce78a6a477e82a224f8428b5c0aab5fe14
Tomás Marques <tomascpmarques@gmail.com>
2024-03-17 23:28:07 +0000

    Turso and local testing db setup
    Schema and local Sqlite instance

--- database/schemas/dump.sql
+++
@@ -1,9 +0,0 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS user(
-  id blob primary key,
-  name Text not null,
-  email Text not null,
-  password Text not null
-);
-COMMIT;
