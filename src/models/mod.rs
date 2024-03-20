3c668772bd55ce004951c54be101d04c4e93d521
Tomás Marques <tomascpmarques@gmail.com>
2024-03-20 00:14:57 +0000

    Added UserSession representation
    Can now represent user sessions

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,8 @@
-#[derive(Debug, serde::Deserialize, serde::Serialize, getset::Getters)]
+#[derive(Debug, serde::Deserialize, serde::Serialize, getset::Getters, getset::MutGetters)]
 #[getset(get = "pub")]
 pub struct User {
-    id: uuid::Uuid,
+    #[getset(get_mut = "pub")]
+    id: Option<uuid::Uuid>,
     name: String,
     email: String,
     password: String,
@@ -9,12 +10,45 @@ pub struct User {
 
 impl User {
     pub fn new(name: String, email: String, password: String) -> Self {
-        let id = uuid::Uuid::new_v4();
         Self {
-            id,
+            id: None,
             name,
             email,
             password,
+        }
+    }
+}
+
+pub fn serialize_utc_to_rfc_3339<'a, S>(
+    time: &'a chrono::DateTime<chrono::Utc>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let retn = time.to_rfc3339();
+    serializer.serialize_str(retn.as_str())
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize, getset::Getters)]
+#[getset(get = "pub")]
+pub struct UserSession {
+    pub id: uuid::Uuid,
+    pub user_id: uuid::Uuid,
+    #[serde(serialize_with = "serialize_utc_to_rfc_3339")]
+    pub expires_in: chrono::DateTime<chrono::Utc>,
+}
+
+impl UserSession {
+    pub fn new(
+        id: uuid::Uuid,
+        user_id: uuid::Uuid,
+        expires_in: chrono::DateTime<chrono::Utc>,
+    ) -> Self {
+        Self {
+            id,
+            user_id,
+            expires_in,
         }
     }
 }
